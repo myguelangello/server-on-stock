086b23949634d88fc398a81a59c99f60b02f005f
Myguel Angello <myguelangello624@gmail.com>
2022-10-27 18:27:15 -0300

    Delete unnecessary files

--- src/routes.ts
+++ src/routes.ts
@@ -1,27 +1,10 @@
 import { Router } from 'express';
-<<<<<<< HEAD
 import { AuthController } from './controllers/User/AuthController';
-=======
-import { UserController } from './controllers/User/UserController';
->>>>>>> master
+import { CreateUserController } from './controllers/User/CreateUserController';
 
-const userControl = new UserController();
 const router = Router();
 
-<<<<<<< HEAD
 router.post('/users', new CreateUserController().handle)
 router.post('/login', new AuthController().handle)
-=======
-
-router.post('/signup', userControl.createUser)
-router.post('/login', userControl.loginUser)
-
-router.get('/', (request, response)=>{
-    response.send("Server is running on port 3000");
-})
-
-router.get('/allUser', userControl.allUser);
-
->>>>>>> master
 
 export { router }
